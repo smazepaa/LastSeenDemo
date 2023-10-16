4828e84d77208227b3e78b8ef3f7e88a6adbdcea
smazepaa <smazepa@kse.ord.ua>
2023-10-16 16:54:51 +0300

    #2, #3 calculate min, max daily online

---
+++ LastSeenDemo/MinMaxDaily.cs
@@ -0,0 +1,24 @@
+﻿namespace LastSeenDemo;
+
+
+public class MinMaxDaily
+{
+    public (double, double) CalculateMinMax(List<UserTimeSpan> value, DateTimeOffset from, DateTimeOffset to)
+    {
+        var listOnline = new List<double>();
+
+        while (from <= to)
+        {
+            var dateTimeProvider = new DateTimeProvider();
+            var onlineDetector = new OnlineDetector(dateTimeProvider);
+            double dailyOnlineTime = onlineDetector.CalculateTotalTimeForUser(value, from, from.AddDays(1));
+
+            listOnline.Add(dailyOnlineTime);
+
+            from = from.AddDays(1);
+        }
+
+        listOnline.Sort(); // sorted in ascending order
+        return (listOnline[0], listOnline[-1]);
+    }
+}
