23c0c933a8d881d4dd85d7194b70c54a96895a95
smazepaa <smazepa@kse.ord.ua>
2023-10-16 18:02:57 +0300

    #6, #7 MinMax unit tests

--- LastSeenDemo/MinMaxDaily.cs
+++ LastSeenDemo/MinMaxDaily.cs
@@ -3,22 +3,24 @@
 
 public class MinMaxDaily
 {
+    private readonly OnlineDetector _onlineDetector;
+    public MinMaxDaily(OnlineDetector onlineDetector)
+    {
+        _onlineDetector = onlineDetector;
+    }
+
+    
     public (double, double) CalculateMinMax(List<UserTimeSpan> value, DateTimeOffset from, DateTimeOffset to)
     {
         var listOnline = new List<double>();
-
         while (from <= to)
         {
-            var dateTimeProvider = new DateTimeProvider();
-            var onlineDetector = new OnlineDetector(dateTimeProvider);
-            double dailyOnlineTime = onlineDetector.CalculateTotalTimeForUser(value, from, from.AddDays(1));
-
+            double dailyOnlineTime = _onlineDetector.CalculateTotalTimeForUser(value, from, from.AddDays(1));
             listOnline.Add(dailyOnlineTime);
-
             from = from.AddDays(1);
         }
 
         listOnline.Sort(); // sorted in ascending order
-        return (listOnline[0], listOnline[-1]);
+        return (listOnline[0], listOnline[^1]);
     }
 }
